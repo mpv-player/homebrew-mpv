0684f8bfe00fb5795150e3fb2445fbd510dbf7ae
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-05-10 09:30:08 +0200

    add lcms2 dependency to mplayer2-wm4

--- Formula/mplayer2-wm4.rb
+++ Formula/mplayer2-wm4.rb
@@ -5,6 +5,8 @@ class Mplayer2Wm4 < Formula
   include Mplayer2Base
   head 'git://git.mplayer2.org/wm4/mplayer2.git', :using => :git
 
+  depends_on 'lcms2' => :build
+
   private
   def binary_name
     'mplayer2-wm4'
