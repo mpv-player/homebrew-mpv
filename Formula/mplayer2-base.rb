43a36683ed9537e35d8041886a63f864fd7c0498
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-05-10 09:06:08 +0200

    add wm4 repository, DRY up both formulas using a mixin

---
+++ Formula/mplayer2-base.rb
@@ -0,0 +1,41 @@
+module Mplayer2Base
+  def self.included(base)
+    base.class_eval do
+      homepage 'http://mplayer2.org'
+
+      depends_on 'pkg-config' => :build
+      depends_on 'libbs2b' => :build
+      depends_on 'libass' => :build
+      depends_on 'homebrew/dupes/freetype' => :build
+      depends_on 'pigoz/mplayer2/libav' => :build
+    end
+  end
+
+  def install
+    args = ["--prefix=#{prefix}",
+            "--cc=#{ENV.cc}",
+            "--enable-macosx-bundle",
+            "--enable-macosx-finder",
+            "--enable-apple-remote"]
+
+    generate_version
+    system "./configure", *args
+    system "make install"
+
+    mv bin + 'mplayer', bin + binary_name
+  end
+
+  private
+  def generate_version
+    ohai "Generating VERSION from the Homebrew's git cache"
+    File.open('VERSION', 'w') {|f| f.write(git_revision) }
+  end
+
+  def git_revision
+    `cd #{git_cache} && git describe --match "v[0-9]*" --always`.strip
+  end
+
+  def git_cache
+    @downloader.cached_location
+  end
+end
--- Formula/mplayer2-wm4.rb
+++ Formula/mplayer2-wm4.rb
@@ -1,12 +1,12 @@
 require 'formula'
 require File.join(File.dirname(__FILE__), 'mplayer2-base.rb')
 
-class Mplayer2 < Formula
+class Mplayer2Wm4 < Formula
   include Mplayer2Base
-  head 'git://git.mplayer2.org/mplayer2.git', :using => :git
+  head 'git://git.mplayer2.org/wm4/mplayer2.git', :using => :git
 
   private
   def binary_name
-    'mplayer2'
+    'mplayer2-wm4'
   end
 end
