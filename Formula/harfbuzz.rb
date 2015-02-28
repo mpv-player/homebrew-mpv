2d8510e605e296ccaa9a70eeb670954a67b82165
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2015-02-28 23:37:50 +0100

    harfbuzz: remove unneeded dependencies
    For our purposes, glib and icu4c are not needed (harfbuzz can use it's own
    bundled Unicode library instead).

--- Formula/harfbuzz.rb
+++ Formula/harfbuzz.rb
@@ -4,13 +4,10 @@ class Harfbuzz < Formula
   sha256 "6736f383b4edfcaaeb6f3292302ca382d617d8c79948bb2dd2e8f86cdccfd514"
 
   depends_on "pkg-config" => :build
-  depends_on "glib"
-  depends_on "icu4c" => :recommended
   depends_on "freetype"
 
   def install
     args = %W[--disable-dependency-tracking --prefix=#{prefix}]
-    args << "--with-icu" if build.with? "icu4c"
     system "./configure", *args
     system "make", "install"
   end
