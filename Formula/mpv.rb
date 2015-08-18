53c9aab04497443fe5cf49e3b78d42ff99ae1614
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2015-08-18 15:51:36 +0200

    mpv: make youtube-dl recommended
    Fixes #66

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -33,6 +33,7 @@ class Mpv < Formula
   depends_on 'libdvdnav'   => :optional
   depends_on 'little-cms2' => :recommended
   depends_on 'lua'         => :recommended
+  depends_on 'youtube-dl'  => :recommended
   depends_on 'libbluray'   => :optional
   depends_on 'libaacs'     => :optional
   depends_on :x11          => :optional
