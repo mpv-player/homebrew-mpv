98a4a337d060f8f233e3f751c813555ed1174eb5
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2015-12-20 13:36:21 +0100

    mvtools: fix stable release building

--- Formula/mvtools.rb
+++ Formula/mvtools.rb
@@ -4,20 +4,18 @@ class Mvtools < Formula
   url "https://github.com/dubhater/vapoursynth-mvtools/archive/v9.tar.gz"
   homepage "https://github.com/dubhater/vapoursynth-mvtools"
   head "https://github.com/dubhater/vapoursynth-mvtools.git"
+  sha256 "e417764cddcc2b24ee5a91c1136e95237ce1424f5d7f49ceb62ff092db18d907"
 
   depends_on "pkg-config" => :build
   depends_on "yasm" => :build
   depends_on "vapoursynth"
   depends_on "fftw"
-
-  if build.head?
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def install
-    system "./autogen.sh" if build.head?
+    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
